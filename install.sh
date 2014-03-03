ecc394d2e83a7539d6cbeb7ba0529bf9cbb0e27d
Tarik <tarik.amar@gmail.com>
2014-03-03 23:22:30 +0100

    Add todo to script install

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # Create .vimrc symlink on home from folder .vim
+# TODO Add option backup
 ln -s $HOME/.vim/.vimrc $HOME
 
 # Init and pull submodules plugins
 git submodule init
 git submodule update
+
+# TODO Install with Vundle
