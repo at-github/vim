cfbd7240e590e2267ae1fe0b207467ff146afdea
Tarik <tarik.amar@gmail.com>
2014-11-22 18:35:47 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 # Create .vimrc symlink on home from folder .vim
 # TODO Add option backup
-ln -s $HOME/.vim/.vimrc $HOME
+ln -fs $HOME/.vim/.vimrc $HOME
 
 # Init and pull submodules plugins
+cd $HOME/.vim
 git submodule init
 git submodule update
+cd -
 
 vim +BundleInstall +qall
